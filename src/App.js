5d14c44f9743fc1b7b59154b12f526e2e5237eab
fikrinazilul1690 <fikrinazilul@gmail.com>
2022-12-01 07:02:22 +0700

    modify App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route, Navigate, Outlet } from 'react-router-dom';
+import Posts from './pages/Posts';
+import PostDetail from './pages/PostDetail';
+import About from './pages/About';
+import Users from './pages/Users';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<Navigate to={'/posts'} replace />} />
+      <Route path='posts' element={<Outlet />}>
+        <Route path=':postId' element={<PostDetail />} />
+        <Route index element={<Posts />} />
+      </Route>
+      <Route path='users' element={<Users />} />
+      <Route path='about' element={<About />} />
+    </Routes>
   );
 }
 
